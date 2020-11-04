40a7e3611a4d8dd9aa512b49d8d49e44f03f6aad
Diogo Pina Manique <diogoprpm@gmail.com>
2020-11-04 12:08:16 +0000

    initial commit

---
+++ app/src/main/java/pt/ipbeja/pdm/todoapp/MainActivity.java
@@ -0,0 +1,19 @@
+package pt.ipbeja.pdm.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
